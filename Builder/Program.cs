34867236837165326fa945f1818edc728432d7ac
Nikolaos Kladis <nikcladis@gmail.com>
2025-03-06 12:45:56 +0200

    🚚 Refactor: Move files to project root

---
+++ Builder/Program.cs
@@ -0,0 +1,16 @@
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+garage.Show();
+
+garage.Construct(bmwBuilder);
+garage.Show();
+
+Console.ReadKey();
