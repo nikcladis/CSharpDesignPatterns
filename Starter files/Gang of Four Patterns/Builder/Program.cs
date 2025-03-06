b09188fb3fe8e948b74ada0d60de1f348549c293
Nikolaos Kladis <nikcladis@gmail.com>
2025-03-06 02:49:11 +0200

    📐 Pattern: Builder

--- Starter files/Gang of Four Patterns/Builder/Program.cs
+++ Starter files/Gang of Four Patterns/Builder/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.Title = "Builder";
+﻿using BuilderPattern;
+
+Console.Title = "Builder";
+
+var garage = new Garage();
+
+var miniBuilder = new MiniBuilder();
+var bmwBuilder = new BMWBuilder();
+
+garage.Construct(miniBuilder);
+garage.Show();
+
+garage.Construct(bmwBuilder);
+garage.Show();
+
+Console.ReadKey();
