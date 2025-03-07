a075c4a63e296af6f0d126677a118427e2630b0b
Nikolaos Kladis <nikcladis@gmail.com>
2025-03-07 18:03:36 +0200

    📐 Pattern: Adapter

--- Adapter/Program.cs
+++ Adapter/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.Title = "Adapter";
+﻿using Adapter;
+
+Console.Title = "Adapter";
+
+ICityAdapter adapter = new CityAdapter();
+var city = adapter.GetCity();
+
+Console.WriteLine($"City: {city.FullName}, Inhabitants: {city.Inhabitants}");
+Console.ReadKey();
